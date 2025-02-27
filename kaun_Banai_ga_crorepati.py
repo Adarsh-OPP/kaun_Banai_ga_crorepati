8ca1f9619506a3673e53ef9b38dfd7ec44a95a53
Adarsh Kumar <adarshkmrina@gmail.com>
2025-02-27 12:07:47 +0530

    Update kaun_Banai_ga_crorepati.py

--- kaun_Banai_ga_crorepati.py
+++ kaun_Banai_ga_crorepati.py
@@ -29,16 +29,14 @@ for i in range(0, len(questions)):
   print(f"c. {question[3]}          d. {question[4]} ")
   reply = int(input("Enter your answer (1-4) or  0 to quit:\n" ))
   if (reply == 0):
-    if (i < 4):
-        money = 0 
-        break
-    else:
-        money = levels[i-1]
-        break
+      money = levels[i-1]
+      break
 
-  if(reply == question[-1]):
+  elif(reply == question[-1]):
     print(f"Correct answer, you have won Rs. {levels[i]}")
-    if(i == 4):
+    if (i < 4):
+      money = 0 
+    elif(i == 4):
       money = 10000
     elif(i == 9):
       money = 320000
